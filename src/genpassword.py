3674b88a13c3869787b70ea4e84d8cb36e6508ac
Mikhail_Mrikh <158068110+Mikhail-Mrikh@users.noreply.github.com>
2024-05-18 18:13:50 +0700

    issue-3 completed

--- src/genpassword.py
+++ src/genpassword.py
@@ -1,10 +1,4 @@
-from src.lib import generate_multiple_random_words
+from src.lib import main_menu
 
 if __name__ == "__main__":
-
-    length = int(input("Введите длину строки: "))
-    count = int(input("Введите количество строк: "))
-
-    random_words = generate_multiple_random_words(length, count)
-    for word in random_words:
-        print(word)
+    main_menu()
